66da683224c628c0ee5d4dba73cf89f263435c74
Dariia10 <dariia.martovytska@example.com>
2022-10-09 19:27:22 +0300

    add

--- thirdwindow.py
+++ thirdwindow.py
@@ -7,7 +7,6 @@ class ThirdWindow(QWidget):
         super().__init__()
         self.set_appear()
         self.initUI()
-        self.connects()
         self.show()
 
     def set_appear(self):
@@ -16,8 +15,11 @@ class ThirdWindow(QWidget):
         self.move(win_x, win_y)
 
     def initUI(self):
-        pass
+        self.lb_work = QLabel('робота спасобность')
+        self.lb_index = QLabel('index')
+        self.v_line = QVBoxLayout()
+        self.v_line.addWidget(self.lb_index, alignment = Qt.AlignCenter)
+        self.v_line.addWidget(self.lb_work, alignment = Qt.AlignCenter)
+        self.setLayout(self.v_line)
 
 
-    def connects(self):
-        pass
